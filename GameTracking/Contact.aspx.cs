2a92e0ce57640ca00734f8a2f4fa668015b7eabd
Ritesh Patel <patelritesh261@outlook.com>
2016-06-05 12:25:40 -0400

    Add project files.

---
+++ GameTracking/Contact.aspx.cs
@@ -0,0 +1,81 @@
+﻿using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/*
+* @File name : Contact 
+* @Author : Ritesh Patel
+* @Website name : 
+* @File description : Contact form and google map 
+* @send mail through sendgrid
+* */
+namespace GameTracking
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /**
+               *Mehod for sending mail
+               * 
+               * @method send_mail
+               * @retyrn {boolean} return true on success
+               * */
+
+                Boolean flag = send_mail();
+                if (flag)
+                {
+                    txtfirstname.Text = "";
+                    txtEmail.Text = "";
+                    txtMessage.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private bool send_mail()
+        {
+            Boolean flag = false;
+
+
+
+            // Create the email object first, then add the properties.
+            try
+            {
+                SendGridMessage myMessage = new SendGridMessage();
+                myMessage.AddTo(txtEmail.Text.ToString());
+                myMessage.From = new MailAddress("testwebritz@gmail.com", "Ritesh Patel");
+                myMessage.Subject = "Contact from portfolio";
+                myMessage.Text = " Hi " + txtfirstname.Text.ToString().Trim() + "\n\n Thank you for reviewing my portfolio\n\n Thank you. ";
+
+                // Create a Web transport, using API Key
+                var transportWeb = new Web("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");
+
+                // Send the email.
+                transportWeb.DeliverAsync(myMessage);
+                // NOTE: If your developing a Console Application, use the following so that the API call has time to complete
+                // transportWeb.DeliverAsync(myMessage).Wait();
+                flag = true;
+            }
+            catch (Exception) { }
+            return flag;
+
+
+        }
+    }
+}
---
+++ GameTracking/User_Controls/Footer.ascx
@@ -0,0 +1,48 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Footer.ascx.cs" Inherits="GameTracking.User_Controls.Footer" %>
+  <!-- start footer -->
+    <footer class="footer-distributed">
+        <div class="footer-left">
+            <h3>
+                <img src="Assets/images/logo.png" /></h3>
+            <p class="footer-links">
+                <a href="Default.aspx">Home</a>
+                ·
+         <a href="Projects.aspx">Projects</a>
+                ·
+         <a href="about.aspx">About</a>
+                ·
+         <a href="Education.aspx">Education</a>
+                ·
+         <a href="Service.aspx">Skills</a>
+                ·
+         <a href="Contact.aspx">Contact</a>
+            </p>
+            <p class="footer-company-name">Ritz &copy; 2016</p>
+        </div>
+        <div class="footer-center">
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>10 vanessa drive, ON L3V 7Y9</span> Orillia, Canada</p>
+            </div>
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+1 705 500 4460</p>
+            </div>
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:support@company.com">200307232@student.georgianc.on.ca</a></p>
+            </div>
+        </div>
+        <div class="footer-right">
+            <p class="footer-company-about">
+                <span>Social Contact</span>
+            </p>
+            <div class="footer-icons">
+                <a href="https://www.facebook.com/groups/234370450229319/" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="https://twitter.com/patelritesh2611" target="_blank"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/ritesh-patel-5b147b6a?trk=nav_responsive_tab_profile" target="_blank"><i class="fa fa-linkedin"></i></a>
+                <a href="https://github.com/patelritesh261/portfolio" target="_blank"><i class="fa fa-github"></i></a>
+            </div>
+        </div>
+    </footer>
+    <!-  end footer -->
