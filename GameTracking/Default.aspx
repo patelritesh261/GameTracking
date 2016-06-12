63df7e550cb05339f27d8ad12c44cb51d2b05e65
Ritesh Patel <patelritesh261@outlook.com>
2016-06-12 13:06:23 -0400

    Home Page Done

--- GameTracking/Default.aspx
+++ GameTracking/Default.aspx
@@ -1,4 +1,5 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GameTracking.Default" %>
+
 <%-- 
  * @File name : Home page 
  * @Author : Ritesh Patel and Parvati Patel
@@ -6,12 +7,80 @@
  * @File description : This is public home page.
  * 
  *  
-    --%>
+--%>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <bs3:Jumbotron runat="server" ID="Jumbotron1">
-        <BodyContent>
-            <h1>Welcome!</h1>
-        </BodyContent>
-    </bs3:Jumbotron>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-1 col-md-11">
+                   <h1>Weekly Statistic</h1>
+                <asp:Repeater ID="rptGame" runat="server" > 
+					<ItemTemplate >	
+                           <div class="col-md-5 card text-xs-center">
+                    <div class="card-header">
+                       
+                        <asp:LinkButton ID="lbGame" runat="server" OnClick="lbGame_Click" Text='<%# Eval("GName") %>' ></asp:LinkButton>
+                       
+                    </div>
+                    <div class="card-block">
+                        <h4 class="card-title">
+                             <asp:LinkButton ID="lbTeam1" runat="server" OnClick="lbTeam1_Click" Text='<%# Eval("TeamN1") %>' ></asp:LinkButton>
+                             V/S 
+                             <asp:LinkButton ID="lbTeam2" runat="server" OnClick="lbTeam2_Click" Text='<%# Eval("TeamN2") %>' ></asp:LinkButton>
+                         </h4>
+                        <div class="card-text">
+                            <p>Winner Team : <%# Eval("WTeamN") %></p>
+                            <p>Spectators : <%# Eval("Sepectators") %></p>
+                            <p>Total Score : <%# Eval("TotalScore") %></p>
+                            <p>Winning Team Points : <%# Eval("T1WinScore") %> and Team won by : <%# Eval("T2WinScore") %> Points</p>
+                            <p>Loosing Team Points :<%# Eval("T2WinScore") %> and Team lost by : <%# Eval("T1WinScore") %> Points</p>
+                        </div>
 
+                    </div>
+                    <div class="card-footer text-muted">
+                     <%# Eval("Date","{0:MMM dd, yyyy}") %> - Week <%# Eval("Week") %>
+                    </div>
+                </div>
+                    </ItemTemplate>
+                   
+                   
+                </asp:Repeater>
+              <div>
+                  <div class="col-md-1">
+                <asp:Button ID="btnprevious" runat="server" Text="<i class='fa fa-arrow-left fa-5x'></i>" />
+                      </div>
+              </div>
+             
+               
+            </div>
+
+        </div>
+    </div>
+       <div class="modal fade" id="myModal" tabindex="-1" role="dialog"
+            aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close"
+                            data-dismiss="modal" aria-hidden="true">
+                            &times;
+                        </button>
+                        <h4 class="modal-title" id="myModalLabel">
+                            <asp:Label ID="lblTitle" runat="server"></asp:Label>
+                        </h4>
+                    </div>
+                    <div class="modal-body">
+                       <asp:Label ID="lblbody" runat="server"></asp:Label>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default"
+                            data-dismiss="modal">
+                            close
+                        </button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal -->
+        </div>
 </asp:Content>
--- GameTracking/Default.aspx.cs
+++ GameTracking/Default.aspx.cs
@@ -4,21 +4,118 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+// using statements that are required to connect to EF DB
+using GameTracking.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+using System.Globalization;
 /*
- *  * @File name : Home page 
- * @Author : Ritesh Patel and Parvati Patel
- * @Website name : GameTracking(http://gametracking.azurewebsites.net/)
- * @File description : This is public home page.
- * 
- * 
- */
+*  * @File name : Home page 
+* @Author : Ritesh Patel and Parvati Patel
+* @Website name : GameTracking(http://gametracking.azurewebsites.net/)
+* @File description : This is public home page.
+* 
+* 
+*/
 namespace GameTracking
 {
     public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //bind data to form
+                bindGames();
+            }
+        }
+        /**
+       * <summary>
+       * This method gets the games data from the DB
+       * </summary>
+       * 
+       * @method bindGames
+       * @returns {void}
+       */
+        private void bindGames()
+        {
+            //Connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //get current week
+                CultureInfo ciCurr = CultureInfo.CurrentCulture;
+                int Week = Convert.ToInt32(ciCurr.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday).ToString());
 
+
+                //write query
+                var GameRecords = (from GR in db.GameRecords1
+                                   join G in db.Games1 on GR.Gid equals G.GID
+                                   join T in db.Teams1 on GR.Team1 equals T.TID
+                                   join T1 in db.Teams1 on GR.Team2 equals T1.TID
+                                   join W in db.Teams1 on GR.WTeam equals W.TID
+                                   where GR.Week==Week
+                                   select new { GR.GRID, GR.Sepectators, GR.T1WinScore, GR.T2WinScore, GR.Date, GR.Team2, GR.Team1, GR.WTeam, GName = G.Name, TeamN1 = T.Name, TeamN2 = T1.Name, WTeamN = W.Name, TotalScore=GR.T1WinScore+GR.T2WinScore,GR.Week });
+
+                //bind result to grid view
+                rptGame.DataSource = GameRecords.ToList();
+                rptGame.DataBind();
+                
+            }
+        }
+
+        protected void lbGame_Click(object sender, EventArgs e)
+        {
+            // find link button and createe object
+            LinkButton lnkBtnTags = (LinkButton)sender;
+            //Connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                var recordGame = (from record in db.Games1
+                                  where record.Name == lnkBtnTags.Text.Trim()
+                                  select record).FirstOrDefault();
+
+                //bind data to model pop up
+                lblTitle.Text = recordGame.Name;
+                lblbody.Text = recordGame.Description;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "openModal();", true);
+            }
+                
+        }
+
+        protected void lbTeam1_Click(object sender, EventArgs e)
+        {
+            // find link button and createe object
+            LinkButton lnkBtnTags = (LinkButton)sender;
+            //Connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                var recordGame = (from record in db.Teams1
+                                  where record.Name == lnkBtnTags.Text.Trim()
+                                  select record).FirstOrDefault();
+
+                //bind data to model pop up
+                lblTitle.Text = recordGame.Name;
+                lblbody.Text = recordGame.Description;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "openModal();", true);
+            }
+        }
+
+        protected void lbTeam2_Click(object sender, EventArgs e)
+        {
+            // find link button and createe object
+            LinkButton lnkBtnTags = (LinkButton)sender;
+            //Connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                var recordGame = (from record in db.Teams1
+                                  where record.Name == lnkBtnTags.Text.Trim()
+                                  select record).FirstOrDefault();
+
+                //bind data to model pop up
+                lblTitle.Text = recordGame.Name;
+                lblbody.Text = recordGame.Description;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "openModal();", true);
+            }
         }
     }
 }
