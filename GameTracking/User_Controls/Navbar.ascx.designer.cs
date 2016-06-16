56a5c887415907484de24eb8ad4b1fff2e3fb105
Ritesh Patel <patelritesh261@outlook.com>
2016-06-15 21:16:12 -0400

    Added admin link

--- GameTracking/User_Controls/Navbar.ascx.designer.cs
+++ GameTracking/User_Controls/Navbar.ascx.designer.cs
@@ -31,6 +31,15 @@ namespace GameTracking {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl contact;
         
         /// <summary>
+        /// Li1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Li1;
+        
+        /// <summary>
         /// login control.
         /// </summary>
         /// <remarks>
